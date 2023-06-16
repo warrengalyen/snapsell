359e17d5e5a3acd957627fc0d125d925f8f56657
Warren Galyen <wgalyen@hotmail.com>
2023-06-16 09:48:57 -0700

    ci(cypress tests with github actions): setup cypress testing and initial github actions

--- storefront/src/pages/[storeUrl]/index.tsx
+++ storefront/src/pages/[storeUrl]/index.tsx
@@ -1,18 +1,18 @@
-import MainLayout from '@/layouts/MainLayout'
+import MainLayout from '@/layouts/MainLayout';
 
 function StoreHome() {
-    return (
-        <>
-            <p>Store home</p>
-        </>
-    )
+  return (
+    <>
+      <p>Store home</p>
+    </>
+  );
 }
 
 
 export default function() {
-    return (
-        <MainLayout title="name">
-            <StoreHome />
-        </MainLayout>
-    )
+  return (
+    <MainLayout title='name'>
+      <StoreHome />
+    </MainLayout>
+  );
 }
