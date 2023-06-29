3da55710a6eb644c8c0e7e7f6e5237e7898d3066
Warren Galyen <wgalyen@hotmail.com>
2023-06-29 09:13:06 -0700

    fix(fix admin layout): fix admin layout

--- apps/admin/pages/admin/[storeUrl]/orders.tsx
+++ apps/admin/pages/admin/[storeUrl]/orders.tsx
@@ -1,6 +1,49 @@
 import AdminLayout from '../../../layouts/AdminLayout';
+import Table from '../../../components/Table';
+//import findOrders from '../../api/orders';
+import { useRouter } from 'next/router';
+import { useContext, useEffect, useState } from 'react';
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { Order } from 'database';
+
+// type Order = {
+//   order_id: string,
+//   order_number: string;
+//   customer_name: string;
+//   total_items: string;
+//   order_total: string;
+//   date_placed: string;
+// };
 
 function Orders() {
+  const router = useRouter();
+
+  const {
+    data: orders,
+    isLoading,
+    isError,
+  }: UseQueryResult<Record<string, string>> = useQuery({
+    queryKey: ['order'],
+    queryFn: () => fetch('/api/orders').then((res) => res.json()),
+    enabled: !!router.isReady,
+    initialData: {
+      id: '',
+      order_id: '', //order_id
+      friendly_order_number: 'xxxxxxx', //friendly_order_number
+      customer_first_name: '', //customer_first_name, customer_last_name
+      order_details: '', //order_details. quantity
+      total_order_cost: '', //total_order_cost
+      created_at: '', //created_at
+    },
+  });
+
+  const [formOrders, setFormOrders] = useState({});
+
+  useEffect(() => {
+    console.log('orders: ' + JSON.stringify(orders));
+    setFormOrders(orders);
+  }, [orders]);
+
   return (
     <>
       <p>Orders</p>
--- packages/database/prisma/migrations/20230627200752_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `store_owner` on the `Store` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Store" DROP COLUMN "store_owner";
