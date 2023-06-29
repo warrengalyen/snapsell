8a37dc36be626769dd9ed8a8d7ad9380aa3e199a
Warren Galyen <wgalyen@hotmail.com>
2023-06-29 10:31:41 -0700

    feat: cart global state refactor

--- apps/storefront/components/ImageRow.tsx
+++ apps/storefront/components/ImageRow.tsx
@@ -1,33 +1,36 @@
+import Error from './Error';
+
 type Image = {
-  image_id: string;
-  image_url: string;
-  image_alt: string;
+  id: string;
+  src: string;
+  alt: string;
 };
 
 export default function ImageRow({
-                                   images,
-                                   setCurrentImage,
-                                   currentImage,
-                                 }: {
+  images,
+  setCurrentImage,
+  currentImage,
+}: {
   images: Image[];
   setCurrentImage: React.Dispatch<React.SetStateAction<Image>>;
-  currentImage: { id: string; image: string };
+  currentImage: { id: string; alt: string; src: string };
 }) {
   return (
     <div className="flex items-center justify-start gap-2 overflow-y-auto">
-      {images.map((item) => (
-        <img
-          key={item.image_id}
-          src={item.image_url}
-          alt={item.image_id}
-          width="50px"
-          height="50px"
-          className={`h-[65px] w-[65px] cursor-pointer hover:border-2 border-gray-500 ${
-            currentImage.id === item.image_id && 'border-2 border-gray-500'
-          }`}
-          onClick={() => setCurrentImage(item)}
-        />
-      ))}
+      {images &&
+        images.map((item) => (
+          <img
+            key={item.id}
+            src={item.src}
+            alt={item.alt}
+            width="50px"
+            height="50px"
+            className={`h-[65px] w-[65px] cursor-pointer hover:border-2 border-gray-500 ${
+              currentImage.id === item.id && 'border-2 border-gray-500'
+            }`}
+            onClick={() => setCurrentImage(item)}
+          />
+        ))}
     </div>
   );
 }
