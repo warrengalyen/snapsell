0af178058c50c715900ea242b4090cbf6ec0961b
Warren Galyen <wgalyen@hotmail.com>
2023-06-30 12:45:59 -0700

    feat(admin orders page): orders page now fetching the db

--- apps/admin/pages/admin/[storeUrl]/orders.tsx
+++ apps/admin/pages/admin/[storeUrl]/orders.tsx
@@ -1,19 +1,8 @@
 import AdminLayout from '../../../layouts/AdminLayout';
 import Table from '../../../components/Table';
-//import findOrders from '../../api/orders';
 import { useRouter } from 'next/router';
-import { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
-import { Order } from 'database';
-
-// type Order = {
-//   order_id: string,
-//   order_number: string;
-//   customer_name: string;
-//   total_items: string;
-//   order_total: string;
-//   date_placed: string;
-// };
 
 function Orders() {
   const router = useRouter();
@@ -22,38 +11,43 @@ function Orders() {
     data: orders,
     isLoading,
     isError,
-  }: UseQueryResult<Record<string, string>> = useQuery({
+  }: UseQueryResult<Record<string, any>[]> = useQuery({
     queryKey: ['order'],
-    queryFn: () => fetch('/api/orders').then((res) => res.json()),
+    queryFn: () =>
+      fetch('/api/orders?store_id=store_1').then((res) => res.json()),
     enabled: !!router.isReady,
-    initialData: {
-      id: '',
-      order_id: '', //order_id
-      friendly_order_number: 'xxxxxxx', //friendly_order_number
-      customer_first_name: '', //customer_first_name, customer_last_name
-      order_details: '', //order_details. quantity
-      total_order_cost: '', //total_order_cost
-      created_at: '', //created_at
-    },
+    initialData: [],
   });
 
-  const [formOrders, setFormOrders] = useState({});
+  const [formOrders, setFormOrders] = useState<Record<string, any>[]>([]);
 
   useEffect(() => {
-    console.log('orders: ' + JSON.stringify(orders));
     setFormOrders(orders);
   }, [orders]);
 
   return (
     <>
       <p>Orders</p>
+      <Table
+        link={true}
+        linkProperty="order_id"
+        prependLink={`/admin/${router.query.storeUrl}/order`}
+        tableColumnNames={[
+          { id: 'order_number', name: 'Order Number' },
+          { id: 'customer_name', name: 'Customer Name' },
+          { id: 'total_items', name: 'Total Items' },
+          { id: 'order_total', name: 'Order Total' },
+          { id: 'date_placed', name: 'Date Placed' },
+        ]}
+        tableRows={formOrders}
+      />
     </>
   );
 }
 
 export default function () {
   return (
-    <AdminLayout title="Orders">
+    <AdminLayout title="orders">
       <Orders />
     </AdminLayout>
   );
