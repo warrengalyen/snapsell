8a2c41839e1066b5d9385e7008b0ad06fc990673
Warren Galyen <wgalyen@hotmail.com>
2023-07-01 10:44:21 -0700

    Revert "fix([orderid] page, [productid] api): now it renders the table with the data from database"
    This reverts commit bff6d5fc3b2008c799f6df7921adcc3abbc7b0db.

--- apps/admin/pages/admin/[storeUrl]/order/[orderId].tsx
+++ apps/admin/pages/admin/[storeUrl]/order/[orderId].tsx
@@ -4,88 +4,24 @@ import Button from '../../../../components/Button';
 import Card from '../../../../components/Card';
 import Table from '../../../../components/Table';
 import Address from '../../../../components/Address';
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { getOrder } from '../../../api/order';
+import { getAddress } from '../../../api/address';
+import { getCustomer } from '../../../api/customer';
 import { GetServerSideProps } from 'next';
-import { Order } from 'database';
-import { useQuery } from '@tanstack/react-query';
 
-function OrderDetail({ order }: any) {
-  const router = useRouter();
-
-  // // get bill_address
-  const { data: bill_address } = useQuery({
-    queryKey: ['bill_address'],
-    queryFn: () =>
-      fetch(`/api/address/${order.bill_address_id}`).then((res) => res.json()),
-    enabled: !!order && !order.hasOwnProperty('error'),
-  });
-
-  // // get ship_address
-  const { data: ship_address } = useQuery({
-    queryKey: ['ship_address'],
-    queryFn: () =>
-      fetch(`/api/address/${order.ship_address_id}`).then((res) => res.json()),
-    enabled: !!order && !order.hasOwnProperty('error'),
-  });
-
-  // get customer
-  const { data: customer } = useQuery({
-    queryKey: ['customer'],
-    queryFn: () =>
-      fetch(`/api/customer/${order.customer_id}`).then((res) => res.json()),
-    enabled: !!order && !order.hasOwnProperty('error'),
-  });
-
-  if (!order || order.hasOwnProperty('error'))
-    return (
-      <div>
-        {' '}
-        <p>No order matched</p>
-        <div className="flex justify-end">
-          <Button
-            size="default"
-            appearance="primary"
-            onClick={() => router.back()}
-          >
-            Back
-          </Button>
-        </div>
-      </div>
-    );
-
-  const order_details = order.order_details;
-  const items_ordered = order.order_details.reduce((acc: number, item: any) => {
+function OrderDetail({ data, bill_address, ship_address, customer }: any) {
+  // get order data (from server side props)
+  const order = JSON.parse(data);
+  const order_id = order.friendly_order_number;
+  const { total_order_cost } = order;
+  const { order_details } = order;
+  const items_ordered = order_details.reduce((acc: any, item: any) => {
     return (acc = acc + item.quantity);
   }, 0);
+  const customerData = JSON.parse(customer);
 
-  // get products
-  const products_id = order_details?.map((item: any) => item.productId);
-  const [productDetails, setProductDetails] = useState<any[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    if (products_id && productDetails.length === 0) {
-      setIsLoading(true);
-      const queries = products_id.map((id: string) => {
-        return {
-          querykey: ['id', id],
-          queryFn: () => fetch(`/api/product/${id}`).then((res) => res.json()),
-        };
-      });
-      const promises = queries.map((query: any) => query.queryFn());
-      Promise.all(promises).then((results) => {
-        setProductDetails(results);
-        setIsLoading(false);
-      });
-    }
-  }, [products_id, productDetails]);
-
-  const products_price = order_details?.map((item: any) => item.price);
-  const products_quantity = order_details?.map((item: any) => item.quantity);
-  const products_name = productDetails.map((item) => item.product_name);
-  const products_SKU = productDetails.map((item) => item.SKU);
-
+  //@TODO : set tableColumn with order data
   const tableColumnNames = [
     { id: 'Product_name', name: 'Product name' },
     { id: 'SKU', name: 'SKU' },
@@ -93,39 +29,29 @@ function OrderDetail({ order }: any) {
     { id: 'Quantity', name: 'Quantity' },
     { id: 'Total', name: 'Total' },
   ];
-  const tableRows = products_id.map((id: string, index: number) => {
-    const price = products_price[index];
-    const quantity = products_quantity[index];
-    const name = products_name[index];
-    const sku = products_SKU[index];
-    return {
-      id: id,
-      Product_name: name,
-      SKU: sku,
-      Price: String(price),
-      Quantity: String(quantity),
-      Total: String(price * quantity),
-    };
-  });
-
-  if (!bill_address || !ship_address) return <p>fetching address...</p>;
-  if (!customer) return <p>fetching email...</p>;
-  if (isLoading) return <p>fetching product details...</p>;
+  const tableRows = [
+    {
+      id: '1',
+      Product_name: 'Black T-shirt',
+      SKU: '12345',
+      Price: '30',
+      Quantity: '3',
+      Total: '90',
+    },
+  ];
 
   // render page
   return (
     <>
       <div className="w-1/2 md:w-5/6 lg:w-full">
+        <Heading title={`Order #${order_id}`} type="h1" />
         <div className="flex justify-end">
-          <Button
-            size="default"
-            appearance="primary"
-            onClick={() => router.back()}
-          >
-            Back
-          </Button>
+          <Link href="/admin/storeurl/order/">
+            <Button size="default" appearance="primary">
+              Back
+            </Button>
+          </Link>
         </div>
-        <Heading title={`Order #${order.friendly_order_number}`} type="h1" />
       </div>
       <div className="xl:flex">
         <div className="w-1/2 md:w-5/6 lg:w-full">
@@ -140,7 +66,7 @@ function OrderDetail({ order }: any) {
             <div className="flex justify-between">
               <Heading title="Total Order" type="h3" />
               <div className="mr-28">
-                <Heading title={`$${order.total_order_cost}`} type="h3" />
+                <Heading title={`$${total_order_cost}`} type="h3" />
               </div>
             </div>
           </Card>
@@ -148,7 +74,7 @@ function OrderDetail({ order }: any) {
         <div className="mt-10 mb-10 w-1/2 md:w-5/6 lg:w-full xl:ml-20 xl:mt-0">
           <Card>
             <Heading title="Customer" type="h4" />
-            <p className="mb-4">{customer.customer_email}</p>
+            <p className="mb-4">{customerData.customer_email}</p>
             <Heading title="Ship to" type="h4" />
             <Address
               firstName={ship_address.address_first_name}
@@ -179,25 +105,51 @@ function OrderDetail({ order }: any) {
   );
 }
 
-export default function ({ order }: any) {
+export default function ({
+                           data,
+                           bill_address,
+                           ship_address,
+                           customer,
+                         }: {
+  data: any;
+  bill_address: any;
+  ship_address: any;
+  customer: any;
+}) {
   return (
     <AdminLayout title="Order Details">
-      <OrderDetail order={order} />
+      <OrderDetail
+        data={data}
+        bill_address={bill_address}
+        ship_address={ship_address}
+        customer={customer}
+      />
     </AdminLayout>
   );
 }
 
-export const getServerSideProps: GetServerSideProps<{ order: Order }> = async (
+// get server side props
+export const getServerSideProps: GetServerSideProps<{ data: any }> = async (
   context
 ) => {
   const { orderId } = context.query;
-  const order = await fetch(`http://localhost:3001/api/order/${orderId}`).then(
-    (res) => res.json()
-  );
+  const results = await getOrder(orderId);
+  const data = JSON.stringify(results);
+  const bill_address_id = JSON.parse(data).bill_address_id;
+  const ship_address_id = JSON.parse(data).ship_address_id;
+  const customer_id = JSON.parse(data).customer_id;
+  const [bill_address, ship_address, customer] = await Promise.all([
+    getAddress(bill_address_id),
+    getAddress(ship_address_id),
+    getCustomer(customer_id).then((res) => JSON.stringify(res)),
+  ]);
 
   return {
     props: {
-      order,
+      data,
+      bill_address,
+      ship_address,
+      customer,
     },
   };
 };
