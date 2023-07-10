b1856b71b68de3f34ca6f9a5f75e08a6fd87fce4
Warren Galyen <wgalyen@hotmail.com>
2023-07-10 10:14:46 -0700

    feat(auth admin): check storeURL to user authorization

--- apps/admin/pages/admin/[storeUrl]/order/[orderId].tsx
+++ apps/admin/pages/admin/[storeUrl]/order/[orderId].tsx
@@ -9,6 +9,7 @@ import { useRouter } from 'next/router';
 import { GetServerSideProps } from 'next';
 import { useQuery } from '@tanstack/react-query';
 import { getSession } from 'next-auth/react';
+import PrismaStrUrl from '../../../../utils/storeUrl';
 
 function OrderDetail({ order }: any) {
   const router = useRouter();
@@ -201,7 +202,10 @@ export const getServerSideProps: GetServerSideProps<{ order: any }> = async (
   context
 ) => {
   const session = await getSession(context);
-  if (!session) {
+  const userId = session?.user.id;
+  const currentStoreUrl = context.query.storeUrl
+  const prismaStoreUrl = await PrismaStrUrl(userId);
+  if (!session || currentStoreUrl !== prismaStoreUrl) {
     return {
       redirect: {
         destination: '/',
