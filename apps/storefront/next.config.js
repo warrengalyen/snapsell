81c8a7873b7651d065ccfca567d8686c31e9691d
Warren Galyen <wgalyen@hotmail.com>
2023-07-12 09:35:57 -0700

    refactor(nameslug): change to isr

--- apps/storefront/next.config.js
+++ apps/storefront/next.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ['shared-components'],
-  async redirects() {
-    return [
+  images: {
+    remotePatterns: [
       {
-        source: '/',
-        destination: 'http://localhost:3001',
-        permanent: true,
-        basePath: false,
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '**/**',
       },
-    ];
+    ],
   },
 };
