4da093ce25939d1a2ca29726e18e5e5e84487e7d
Warren Galyen <wgalyen@hotmail.com>
2023-07-13 08:54:36 -0700

    fix(next config): fix syntax on next config

--- apps/storefront/next.config.js
+++ apps/storefront/next.config.js
@@ -11,4 +11,14 @@ module.exports = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'http://localhost:3001',
+        permanent: true,
+        basePath: false,
+      },
+    ];
+  },
 };
