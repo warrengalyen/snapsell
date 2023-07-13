d99c6f8bde26e3f6f57d13cb3922e1443c60fc05
Warren Galyen <wgalyen@hotmail.com>
2023-07-13 08:50:48 -0700

    fix(webhook): add dynamic order total

--- apps/storefront/pages/api/stripe/webhook.ts
+++ apps/storefront/pages/api/stripe/webhook.ts
@@ -12,7 +12,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  console.log('METADATA', Object.entries(req.body.data.object.metadata));
   const {
     email,
     store_url,
@@ -33,19 +32,12 @@ export default async function handler(
     firstName,
     city,
     phoneNumber,
+    total_order,
   } = req.body.data.object.metadata;
 
   const lineItems = Object.entries(req.body.data.object.metadata)
     .filter((item) => item[0].includes('item_'))
     .map((item: any) => JSON.parse(item[1]));
-
-  console.log(lineItems);
-  console.log(
-    'MAP',
-    lineItems.map((item) => ({ product_id: item.id }))
-  );
-
-  console.log(req.body);
 
   try {
     if (req.body.type !== 'charge.succeeded') {
@@ -101,7 +93,6 @@ export default async function handler(
             },
           },
           friendly_order_number: getOrderCountForStore + 1000,
-          total_order_cost: 10,
           payment_id: req.body.data.object.payment_intent,
           order_details: lineItems,
           store: {
